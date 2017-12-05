bc1ab6e145a69ec99a8a36eb4e6ef676f7b1ff1b
Denis <webdevelope2017@gmail.com>
2017-12-05 17:01:56 +0300

    Work done

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const sum = require('./modules/sum');
+const guard = require('./modules/guard');
+const $ = require('jquery');
+
+console.log(sum(13,22));
+console.log(guard('Mario'));
+console.log($);
---
+++ src/modules/guard.js
@@ -0,0 +1,7 @@
+module.exports = function (name) {
+    if(name == 'Mario') {
+        return 'hello, Mario! Come in, please..';
+    } else{
+        return 'Get out!';
+    }
+}
